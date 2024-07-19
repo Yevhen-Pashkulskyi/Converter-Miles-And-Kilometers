c60910e59b98712f5724f278fe1f2e5ac25a467b
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-07-19 09:33:59 +0300

    realisated task

--- src/main/java/ConverterMileAndKilometer.java
+++ src/main/java/ConverterMileAndKilometer.java
@@ -2,7 +2,11 @@ public class ConverterMileAndKilometer {
     private final static Double MILES_PER_INCH = 1.609344;
 
     public static void main(String[] args) {
+        double kilometersToMiles = 100;
+        print(convertKilometersToMiles(kilometersToMiles));
 
+        double milesToKilometers = 13.56;
+        print(convertMilesToKilometers(milesToKilometers));
     }
 
     private static Double convertMilesToKilometers(Double miles) {
@@ -13,7 +17,7 @@ public class ConverterMileAndKilometer {
     }
 
     private static void print(double value) {
-        System.out.println(value + " " + MILES_PER_INCH);
+        System.out.printf("result = %.2f \n", value);
     }
 
 }
