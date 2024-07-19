aa5b9ed7c36dfb0651cf87e00ab860ed9af4cf9f
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-07-19 09:17:49 +0300

    add method print

--- src/main/java/ConverterMileAndKilometer.java
+++ src/main/java/ConverterMileAndKilometer.java
@@ -1,6 +1,12 @@
 public class ConverterMileAndKilometer {
     private final static Double MILES_PER_INCH = 1.609344;
+
     public static void main(String[] args) {
 
     }
+
+    private static String print(double value) {
+        return value + " " + MILES_PER_INCH;
+    }
 }
+
